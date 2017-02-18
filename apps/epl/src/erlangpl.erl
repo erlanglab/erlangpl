ce2516301735ab9400c5a0d4f72125bcfed66e09
Erlang Lab <info@erlang.pl>
2017-02-18 23:33:51 +0100

    Start epl_3d plugin

--- apps/epl/src/erlangpl.erl
+++ apps/epl/src/erlangpl.erl
@@ -17,6 +17,7 @@ main(_) ->
     ok = application:start(cowboy),
     ok = application:start(ej),
     ok = application:start(epl),
+    ok = application:start(epl_3d),
 
     %% Start applications because escript can't take -boot argument
     %% TODO: start sasl if escript run with debug flags
