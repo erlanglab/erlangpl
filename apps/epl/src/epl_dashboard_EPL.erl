68db0735b8d058caf14aa5caecca91b11ce70fc3
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-02-24 21:33:24 +0100

    Add primitive topic system to WS messages

--- apps/epl/src/epl_dashboard_EPL.erl
+++ apps/epl/src/epl_dashboard_EPL.erl
@@ -38,7 +38,9 @@ init(_Options) ->
 init({tcp, http}, _Req, _Opts) ->
     [{node, Node}] = epl:lookup(node),
     [{node_settings, NodeSettings}] = epl:lookup(node_settings),
-    JSON = epl:proplist_to_json([{node_name, Node}|NodeSettings]),
+    Data = [{<<"topic">>, <<"system-init">>},
+            {node_name, Node}|NodeSettings],
+    JSON = epl:proplist_to_json(Data),
     self() ! {data, JSON},
     epl_dashboard:subscribe(),
     {upgrade, protocol, cowboy_websocket}.
