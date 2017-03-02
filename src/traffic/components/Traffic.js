400097d4ad6b2de2d76422607b1c575a807a3403
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-03-02 23:23:34 +0100

    Add simple loader when graph is not ready

--- src/traffic/components/Traffic.js
+++ src/traffic/components/Traffic.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux';
 import Vizceral from 'vizceral-react';
 import { Motion, spring } from 'react-motion';
 
+import loaderLogo from '../../images/erlanglab_logo_alpha_250.png';
+import loaderText from '../../images/erlangpl_test_1.jpg';
+
 import 'vizceral-react/dist/vizceral.css';
 import './Traffic.css';
 
@@ -13,41 +16,55 @@ import * as actions from '../actions';
 class Traffic extends Component {
   state: {
     start: number,
-    end: number
+    end: number,
+    graph: boolean
   };
+
   constructor(props) {
     super(props);
     this.state = {
       start: 0,
-      end: 0
+      end: 0,
+      graph: false
     };
   }
 
-  handleViewChange(view: Array<string>) {
+  handleViewChange(data: any) {
+    let anim;
+    const { view, graph } = data;
     if (view.length > 0 && this.props.view.length === 0) {
       //open
-      this.setState({ end: 1, start: 0 });
+      anim = { end: 1, start: 0 };
     } else if (this.props.view.length > 0) {
       //stay open
-      this.setState({ end: 1, start: 1 });
+      anim = { end: 1, start: 1 };
     } else {
       //close
-      this.setState({ end: 0, start: 1 });
+      anim = { end: 0, start: 1 };
     }
+
+    this.setState({
+      end: anim.end,
+      start: anim.start,
+      graph: graph !== null
+    });
 
     this.props.updateTrafficView(view);
   }
 
   render() {
-    const sidePanelWidth = 25;
+    const sidePanelWidth = 30;
 
     return (
       <div className="Traffic">
         <TrafficTools className="Traffic-tools" />
         <Motion
-          defaultStyle={{ x: this.state.start }}
-          style={{ x: spring(this.state.end) }}
-          children={({ x }) => (
+          defaultStyle={{ x: this.state.start, y: 1 }}
+          style={{
+            x: spring(this.state.end),
+            y: this.state.graph ? spring(0) : 1
+          }}
+          children={({ x, y }) => (
             <div className="Traffic-container">
               <div
                 className="Traffic-panel"
@@ -56,11 +73,31 @@ class Traffic extends Component {
                 <Vizceral
                   traffic={this.props.data}
                   view={this.props.view}
-                  viewChanged={event => this.handleViewChange(event.view)}
+                  viewChanged={event => this.handleViewChange(event)}
                   showLabels={true}
                   allowDraggingOfNodes={true}
                   targetFramerate={30}
                 />
+                <div
+                  className="loader"
+                  style={{
+                    visibility: y ? 'visible' : 'hidden',
+                    opacity: y
+                  }}
+                >
+                  <div className="loader-img">
+                    <img
+                      src={loaderLogo}
+                      style={{ width: '75px', height: '75px' }}
+                      alt="Erlang Performance Lab"
+                    />
+                    <img
+                      style={{ width: '540px', height: '75px' }}
+                      src={loaderText}
+                      alt="Erlang Performance Lab"
+                    />
+                  </div>
+                </div>
               </div>
               <div
                 className="Traffic-panel text-center"
