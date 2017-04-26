dcf19704b1863b6b567053e7978a49942e4a4568
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-04-26 23:24:56 +0200

    Extract update_timeline function from handle_info

--- apps/epl/src/epl_timeline.erl
+++ apps/epl/src/epl_timeline.erl
@@ -64,37 +64,22 @@ handle_cast({unsubscribe, Pid}, State = #state{subscribers = Subs}) ->
     NextSubs = lists:delete(Pid, Subs),
     {noreply, State#state{subscribers = NextSubs}};
 handle_cast({add, Pid}, State = #state{timelines = Timelines}) when is_pid(Pid) ->
-    T = case lists:any(fun({P, _}) -> P == Pid end, Timelines) of
+    T = case lists:any(fun({P,_,_}) -> P == Pid end, Timelines) of
         true -> Timelines;
-        _    -> [{Pid, track_timeline(Pid)}|Timelines]
+        _    -> [track_timeline(Pid)|Timelines]
         end,
     {noreply, State#state{timelines = T}};
 handle_cast({remove, Pid}, State = #state{timelines = Timelines}) when is_pid(Pid) ->
-    NextTimelines = lists:filter(fun({P, _}) -> P /= Pid end, Timelines),
+    NextTimelines = lists:filter(fun({P,_,_}) -> P /= Pid end, Timelines),
     {noreply, State#state{timelines = NextTimelines}};
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-handle_info({data, _, Data}, State = #state{subscribers = Subs, timelines = Times}) ->
-    {Timelines, NewState} =
-        lists:foldl(fun({Pid, {P, PidList, Changes}}, {Acc1, Acc2}) ->
-                            Merged = get_changes(Data, PidList, Changes),
-                            Map =
-                                #{pid =>
-                                      epl:to_bin(Pid),
-                                  timeline =>
-                                      lists:map(fun({M, S}) ->
-                                                        #{message =>
-                                                              to_string(M),
-                                                          state =>
-                                                              to_string(S)}
-                                                end, Merged)},
-                            Timeline = {P, PidList, Merged},
-                            {[Map|Acc1], [{Pid,Timeline}|Acc2]}
-                    end, {[], []}, Times),
-    JSON = epl_json:encode(#{timelines => Timelines}, <<"timeline-info">>),
+handle_info({data, _, Data}, State = #state{subscribers = Subs, timelines = Timelines}) ->
+    {Maped, Updated} = lists:foldl(fun (T, A) -> update_timeline(T, A, Data) end, {[], []}, Timelines),
+    JSON = epl_json:encode(#{timelines => Maped}, <<"timeline-info">>),
     [Pid ! {data, JSON} || Pid <- Subs],
-    {noreply, State#state{timelines = NewState}}.
+    {noreply, State#state{timelines = Updated}}.
 
 terminate(_Reason, _State) ->
     ok.
@@ -106,6 +91,15 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%%===================================================================
 
+update_timeline({Pid, PidList, Changes}, {Maped, Updated}, Data) ->
+    Merged = merge_changes(Data, PidList, Changes),
+    Map = #{pid => epl:to_bin(Pid),
+            timeline => lists:map(fun({M, S}) ->
+                                          #{message => to_string(M),
+                                            state => to_string(S)}
+                                  end, Merged)},
+    {[Map|Maped], [{Pid, PidList, Merged}|Updated]}.
+
 track_timeline(Pid) ->
     epl_tracer:track_timeline(Pid),
     {Pid, pid_to_list(Pid), []}.
@@ -114,7 +108,7 @@ to_string(Data) ->
     S = io_lib:format("~p",[Data]),
     epl:to_bin(lists:flatten(S)).
 
-get_changes(Data, Pid, Changes) ->
+merge_changes(Data, Pid, Changes) ->
     parse_data(Data, Pid) ++ Changes.
 
 parse_data(Data, Pid) ->
