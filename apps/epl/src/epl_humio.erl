c04e8836c8c5b6409cf47503f53a5c7ad5d1f7c2
Rafał Słota <rafal.slota@erlang-solutions.com>
2017-05-17 17:13:05 +0200

    Minor code cleanup

--- apps/epl/src/epl_humio.erl
+++ apps/epl/src/epl_humio.erl
@@ -26,6 +26,7 @@
 -define(HTTP_POOL_NAME, humio_http).
 -define(HUMIO_API_URL, "https://go.humio.com").
 -define(HUMIO_DATASPACE, "erlangpl").
+-define(HUMIO_SOURCE_NAME, <<"erlangpl">>).
 
 %% ===================================================================
 %% API functions
@@ -48,7 +49,6 @@ handle_cast(Request, _State) ->
     exit({not_implemented, Request}).
 
 handle_info({data, {N, T}, Proplist} = Data, State) ->
-    epl:log(debug, "Humio event ~p: ~p", [{N, T}, Proplist]),
     wpool:cast(?HTTP_POOL_NAME, {?MODULE, handle_event, [Data]}, available_worker),
     {noreply, State};
 handle_info(Info, _State) ->
@@ -62,64 +62,75 @@ code_change(_OldVsn, State, _Extra) ->
 
 handle_event(Data) ->
     epl:log(debug, "Humio event ~p~n", [Data]),
-    URL = ?HUMIO_API_URL ++ "/api/v1/dataspaces/" ++ ?HUMIO_DATASPACE ++ "/ingest",
+    URL = humio_push_url(),
     [{humio_token, Token}] = epl:lookup(humio_token),
     Headers = [
-        {"Authorization", "Bearer " ++ Token},
-        {"Content-type", "application/json"},
-        {"Accept", "application/json"} 
-    ],
+               {"Authorization", "Bearer " ++ Token},
+               {"Content-type", "application/json"},
+               {"Accept", "application/json"} 
+              ],
     HTTPOpts = [],
     Opts = [],
-
     JSON = event_to_json_map(Data),
     Body = jsone:encode(JSON),
-    Res = httpc:request(post, {URL, Headers, "application/json", Body}, HTTPOpts, Opts),
-    epl:log(debug, "Humio HTTP res ~p~n", [Res]),
-    ok.
+    case httpc:request(post, {URL, Headers, "application/json", Body}, HTTPOpts, Opts) of
+        {ok, {{_Proto, 200, _StatusText}, _Headers, _Payload}} ->
+            epl:log(debug, "Humio HTTP: report submitted!~n", []),
+            ok;
+        {ok, {{_Proto, StatusCode, _StatusText}, _Headers, _Payload}} ->
+            epl:log(error, "Humio HTTP: Unable to report event. Invalid HTTP status code ~p~n",
+                    [StatusCode]),
+            {error, {invalid_status, StatusCode}};
+        {error, Reason} ->
+            epl:log(error, "Humio HTTP: Unable to report event. Unknown error ~p~n",
+                    [Reason]),
+            {error, Reason}
+    end.
+
+humio_push_url() ->
+    ?HUMIO_API_URL ++ "/api/v1/dataspaces/" ++ ?HUMIO_DATASPACE ++ "/ingest".
 
 event_to_json_map({data, {Node, Timestamp}, Proplist}) ->
     [
      #{
         tags => #{
           host => atom_to_binary(Node, utf8),
-          source => <<"ErlangPL">>
-        },
+          source => ?HUMIO_SOURCE_NAME
+         },
         events => [#{
-          timestamp => format_timestamp(Timestamp),
-          attributes => format_attributes(Proplist)
-        }]
-     }
+                      timestamp => format_timestamp(Timestamp),
+                      attributes => format_attributes(Proplist)
+                    }]
+      }
     ].
 
+%% Fixme!: timezone?
 format_timestamp(Timestamp) ->
     {{Year, Month, Day}, {Hour, Minute, Second}} = calendar:now_to_datetime(Timestamp),
     list_to_binary(lists:flatten(io_lib:format("~4..0w-~2..0w-~2..0wT~2..0w:~2..0w:~2..0w+00:00",[Year,Month,Day,Hour,Minute,Second]))).
 
 format_attributes(Proplist) ->
-    maps:map(
-      fun(spawn, Spawn) ->
-              epl:to_bin(length(Spawn));
-         (exit, Exit) ->
-              Abnormal = lists:foldl(
-                           fun({_,normal,_}, Acc) -> Acc;
-                              (_, Acc) -> Acc + 1
-                           end,
-                           0, Exit),
-              [{<<"count">>, epl:to_bin(length(Exit))},
-               {<<"abnormal">>, epl:to_bin(Abnormal)}];
-         ('receive', Receive) ->
-              {Count, Sizes} = lists:foldl(
-                                 fun({_,C,S}, {Count, Sizes}) ->
-                                         {Count+C, Sizes+S}
-                                 end,
-                                 {0, 0}, Receive),
-              epl:to_bin(Count);
-         (process_count, Value) ->
-              Value;
-         (memory_total, Value) ->
-              Value;
-         (Item, Value) ->
-              invalid
-      end,
-      maps:from_list(Proplist)).
+    lists:foldl(
+      fun({spawn, Spawn}, MapIn) ->
+              maps:put(spawn, epl:to_bin(length(Spawn)), MapIn);
+         ({exit, Exit}, MapIn) ->
+              _Abnormal = lists:foldl(
+                            fun({_,normal,_}, Acc) -> Acc;
+                               (_, Acc) -> Acc + 1
+                            end,
+                            0, Exit),
+              maps:put(exit, epl:to_bin(length(Exit)), MapIn);
+         ({'receive', Receive}, MapIn) ->
+              {Count, _Sizes} = lists:foldl(
+                                  fun({_,C,S}, {Count, Sizes}) ->
+                                          {Count+C, Sizes+S}
+                                  end,
+                                  {0, 0}, Receive),
+              maps:put('receive', epl:to_bin(Count), MapIn);
+         ({process_count, Value}, MapIn) ->
+              maps:put(process_count, Value, MapIn);
+         ({memory_total, Value}, MapIn) ->
+              maps:put(memory_total, Value, MapIn);
+         (_Unknown, MapIn) ->
+              MapIn
+      end, #{}, Proplist).
