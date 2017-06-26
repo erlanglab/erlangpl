b07ce50a328709b6bfdf23ed440c7542923d90cf
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-06-27 01:21:02 +0200

    Rewrite eplTimelie reducer to plain ES6 from immutablejs for better serialization

--- ui/src/plugins/epl-timeline/reducer.js
+++ ui/src/plugins/epl-timeline/reducer.js
@@ -1,61 +1,58 @@
 // @flow
 import * as type from './actionTypes';
-import { Map } from 'immutable';
 
-export const INITIAL_STATE = Map({
+export const INITIAL_STATE = {
   timelines: [],
   pid: null,
   msg: 0,
   pidPrefix: ''
-});
+};
 
 const reducer = (state: any = INITIAL_STATE, action: any) => {
   if (action.type === type.UPDATE_TIMELINES) {
     const [{ timelines }] = action.payload;
-    return state.set(
-      'timelines',
-      timelines.map(t => ({ ...t, timeline: t.timeline.reverse() }))
-    );
+    return {
+      ...state,
+      timelines: timelines.map(t => ({ ...t, timeline: t.timeline.reverse() }))
+    };
   }
 
   if (action.type === type.SET_CURRENT_PID) {
     const [pid] = action.payload;
-    return state.set('pid', pid).set('msg', 0);
+    return { ...state, pid, msg: 0 };
   }
 
   if (action.type === type.SET_CURRENT_MSG) {
     const [msg] = action.payload;
-    return state.set('msg', msg);
+    return { ...state, msg };
   }
 
   if (action.type === type.SET_INIT) {
     const [{ pid }] = action.payload;
-    return state.set('pidPrefix', pid.replace(/<|>/, '').split('.')[0]);
+    return { ...state, pidPrefix: pid.replace(/<|>/, '').split('.')[0] };
   }
 
   if (action.type === 'PUSH_TIMELINE_PID') {
     const pid = action.pid;
-    return state.update('timelines', timelines =>
-      [
-        {
-          pid,
-          timeline: []
-        }
-      ].concat(timelines)
-    );
+    const timeline = { pid, timeline: [] };
+    return {
+      ...state,
+      timelines: [timeline].concat(state.timelines)
+    };
   }
 
   if (action.type === type.REMOVE_PID) {
     const [pid] = action.payload;
-    return state.update('timelines', timelines =>
-      timelines.filter(t => t.pid !== pid)
-    );
+    return {
+      ...state,
+      timelines: state.timelines.filter(t => t.pid !== pid)
+    };
   }
 
   if (action.type === '@@router/LOCATION_CHANGE') {
     const { pathname } = action.payload;
     const pid = pathname.replace(/\/timeline(\/?)/, '');
-    return state.set('pid', pid).set('msg', 0);
+    return { ...state, pid, msg: 0 };
   }
 
   return state;
