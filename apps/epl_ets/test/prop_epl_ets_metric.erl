85bb185211bc100a696968abf930397d06c198ea
Kacper Mentel <mentel.kk@gmail.com>
2017-07-21 17:39:31 +0200

    Add some property tests for epl_ets_metric module

--- apps/epl_ets/test/prop_epl_ets_metric.erl
+++ apps/epl_ets/test/prop_epl_ets_metric.erl
@@ -17,9 +17,25 @@ prop_splitted_by_pid() ->
     ?FORALL(T, list_traces(), 
             splitted_by_pid(epl_ets_metric:split_traces_by_pid(T))).
 
-prop_splitted_same_len() ->
+prop_splitted_by_tab() ->
+    ?FORALL(T, list_traces(),
+            splitted_by_tab(epl_ets_metric:split_traces_by_tab(T))).
+
+prop_splitted_by_func() ->
+    ?FORALL(T, list_traces(),
+            splitted_by_func(epl_ets_metric:split_traces_by_func(T))).
+
+prop_splitted_by_pid_same_len() ->
     ?FORALL(T, list_traces(),
             length_deep(epl_ets_metric:split_traces_by_pid(T)) == length(T)).
+
+prop_splitted_by_tab_same_len() ->
+    ?FORALL(T, list_traces(),
+            length_deep(epl_ets_metric:split_traces_by_tab(T)) == length(T)).
+
+prop_splitted_by_func_same_len() ->
+    ?FORALL(T, list_traces(),
+            length_deep(epl_ets_metric:split_traces_by_func(T)) == length(T)).
 
 %%====================================================================
 %% Generators
@@ -49,3 +65,17 @@ splitted_by_pid(ListOfLists) ->
 
 is_pid_same(List = [{Pid, _, _, _} | _]) ->
     lists:all(fun({P, _, _, _}) -> P =:= Pid end, List).
+
+splitted_by_tab(ListOfLists) ->
+    Result = [is_tab_same(List) || List <- ListOfLists],
+    lists:all(fun(E) -> E end, Result).
+
+is_tab_same(List = [{_, Tab, _, _} | _]) ->
+    lists:all(fun({_, T, _, _}) -> T =:= Tab end, List).
+
+splitted_by_func(ListOfLists) ->
+    Result = [is_func_same(List) || List <- ListOfLists],
+    lists:all(fun(E) -> E end, Result).
+
+is_func_same(List = [{_, _, Func, _} | _]) ->
+    lists:all(fun({_, _, F, _}) -> F =:= Func end, List).
