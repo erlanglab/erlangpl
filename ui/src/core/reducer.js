476688bdec5b0dc14d0d970e2e63fb6ce33a0490
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-07-27 21:26:59 +0200

    Disable ui state caching

--- ui/src/core/reducer.js
+++ ui/src/core/reducer.js
@@ -2,19 +2,19 @@
 
 import { combineReducers } from 'redux';
 
-import { clearState } from '../localStorage';
-import * as t from './actionTypes';
+// import { clearState } from '../localStorage';
+import * as type from './actionTypes';
 
 export const INITIAL_STATE = 'disconnected';
 
 type State = 'connected' | 'disconnected';
 
 const connection = (state: State = INITIAL_STATE, action: any): State => {
-  if (action.type === t.CONNECTION_OPEN) {
+  if (action.type === type.CONNECTION_OPEN) {
     return 'connected';
   }
 
-  if (action.type === t.CONNECTION_CLOSE) {
+  if (action.type === type.CONNECTION_CLOSE) {
     return 'disconnected';
   }
 
@@ -22,18 +22,18 @@ const connection = (state: State = INITIAL_STATE, action: any): State => {
 };
 
 const node = (state: string = '', action: any): string => {
-  if (action.type === t.SET_NODE) {
+  if (action.type === type.SET_NODE) {
     return action.node;
   }
   return state;
 };
 
-const timestamp = (state: number = 0, action: any): number => {
-  if (action.type === t.SET_TIMESTAMP) {
+const timestamp = (state: number = -1, action: any): number => {
+  if (action.type === type.SET_TIMESTAMP) {
     // NOTE: It's sideeffect in reducer. Not nice but not that harmful
-    if (action.timestamp !== state) {
-      clearState();
-    }
+    // if (action.timestamp !== state) {
+    //   clearState();
+    // }
     return action.timestamp;
   }
   return state;
--- ui/src/store.js
+++ ui/src/store.js
@@ -2,9 +2,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
-import throttle from 'lodash/throttle';
+// import throttle from 'lodash/throttle';
 
-import { loadState, saveState } from './localStorage';
+// import { loadState, saveState } from './localStorage';
 import rootReducer from './reducer';
 
 export const history = createHistory();
@@ -14,19 +14,15 @@ const middleware = routerMiddleware(history);
 // NOTE: We're loading state from localStorage
 // it's returning cached state or undefined so
 // reducers can initialize new state if cached was undefined
-const preservedState = loadState();
+// const preservedState = loadState();
 
 const store = createStore(
   rootReducer,
-  preservedState,
+  null, // preservedState,
   applyMiddleware(middleware),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-store.subscribe(
-  throttle(() => {
-    saveState(store.getState());
-  }, 1000)
-);
+// store.subscribe(throttle(() => saveState(store.getState()), 1000));
 
 export default store;
